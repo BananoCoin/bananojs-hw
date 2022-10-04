f9d707170b4907f06b5bccd3210045d8122a95e2
coranos <coranos@users.noreply.github.com>
2022-10-03 21:06:58 -0500

    1.4.2 debugging webpack errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'production',
@@ -6,5 +7,22 @@ module.exports = {
   output: {
     filename: 'TransportWebUSB.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+    fallback: {
+      'stream': require.resolve('stream-browserify'),
+      'buffer': require.resolve('buffer'),
+    },
   },
 };
